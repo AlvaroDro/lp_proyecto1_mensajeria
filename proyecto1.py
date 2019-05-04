9bfd7009b9a0c72f3e6924f987b3386088a202f3
AlvaroDro <alvaro.droppelmann.d@gmail.com>
2019-05-04 18:46:10 -0300

    cambios menores

--- proyecto1.py
+++ proyecto1.py
@@ -8,15 +8,10 @@ def crearusuario(nombusu,contr,nombr,apell,direc,univ):
 	diccionario={}
 
 	diccionario["Usuario"]=nombusu
-
 	diccionario["Password"]=contr
-
 	diccionario["Nombre"]=nombr
-
 	diccionario["Apellido"]=apell
-
 	diccionario["Dirrecion"]=direc
-
 	diccionario["Casa estudio"]=univ
 
 	#datosguardados = json.dumps(diccionario)
@@ -33,58 +28,35 @@ continuar = True # condicion para el bucle while True = continuar False = salir
 while(continuar):
 
 	print("Bienvenido al servicio de mensajeria")
-
 	print("Presione 'a' si quiere iniciar sesion")
-
 	print("Presione 'b' si quiere crear una nueva cuenta")
-
 	print("Presione 'c' si quiere salir del servicio de mensajeria")
 
 	opt = input("Digite su opcion: ")
 
 
-
 	if opt == 'a':
-
-
 
 		nombreini = input("ingrese su nombre de usuario")
 
 		contraini = input("ingrese su password de usuario")
 
-
-
 	elif opt == 'b':
 
 		nombreusu = input("Ingrese su nombre de usuario\n")
-
 		password = input("Ingrese su password de usuario\n")
-
 		nombrereal = input("Ingrese su nombre\n")
-
 		apellido = input("Ingrese su apellido\n")
-
 		direccion = input("Ingrese su dirrecion\n")
-
 		casaestudio = input("Ingrese su casa de estudio\n")
-
 		
-
 		crearusuario(nombreusu,password,nombrereal,apellido,direccion,casaestudio)
-
-
 
 	elif opt == 'c':
 
-
-
 		continuar = False
 
-
-
 	else:
-
-
 
 		print("Opcion no valida, ingrese una de las opciones mostradas")
 
