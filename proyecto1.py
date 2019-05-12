1cd86cc0c487ab4afe84d470a1217cf1aa4986c0
Lactunkk <christopher_lactunkk@hotmail.com>
2019-05-12 19:41:09 -0400

    mirror update

--- proyecto1.py
+++ proyecto1.py
@@ -116,7 +116,11 @@ def vista_de_sesion(indice_cuenta):
                     opcion = opcion.lower()
                     if opcion == "a":
 
-                        eliminar_mensaje = int(input("Ingrese el numero del mensaje a eliminar: "))
+                        eliminar_mensaje = input("Ingrese el numero del mensaje a eliminar: ")
+
+                        if not eliminar_mensaje.isdigit():
+                            print("Ingreso una letra y no un numero")
+                            continue
                     
                         if int(eliminar_mensaje) <= 0 or int(eliminar_mensaje) > len(bandeja):
                             print("ingrese valores correctos para eliminar")
